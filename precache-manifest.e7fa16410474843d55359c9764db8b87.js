7631b0d8fd70fa90e1b1179dd97b2d198ab583e4
josscg449 <38119088+josscg449@users.noreply.github.com>
2020-05-08 03:22:55 -0600

    Updates

---
+++ precache-manifest.e7fa16410474843d55359c9764db8b87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba2618d802adbe8c2c66ab72b0d022c0",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "20135cccd044e353ad82",
+    "url": "/calculator/static/css/main.d14d9506.chunk.css"
+  },
+  {
+    "revision": "4dfa74abb69485281932",
+    "url": "/calculator/static/js/2.692aab3e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.692aab3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20135cccd044e353ad82",
+    "url": "/calculator/static/js/main.ac88a500.chunk.js"
+  },
+  {
+    "revision": "552c96952084d63edc80",
+    "url": "/calculator/static/js/runtime-main.29434a71.js"
+  },
+  {
+    "revision": "db12fbab06771ba83d847e631e18cb60",
+    "url": "/calculator/static/media/Calculator.db12fbab.jpg"
+  }
+]);
